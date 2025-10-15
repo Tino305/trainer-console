436b5399a1cee65e409719fd84a62b9d2c32225d
Tino305 <55108370+Tino305@users.noreply.github.com>
2025-10-14 21:57:14 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('tc-v5').then(c=>c.addAll(['./','./index.html','./icon.png','./manifest.webmanifest','./cartridges.json'])));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
